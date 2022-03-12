256f331a1c412786942780be96d7d0ddf62b22cb
tlamatqui <tlamatqui@protonmail.com>
2022-03-12 15:53:03 -0800

    add functionality and test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+  let array = arr;
+  let removeValues = remove;
 
+  for (i=0; i<removeValues.length; i++) {
+    for (j=0; j<array.length; j++) {
+      if (array[j] === removeValues[i]) {
+        array.splice(j, 1);
+        j--
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
