d9545742dcc56cc456708f5aaf97e541b1544ab3
tlamatqui <tlamatqui@protonmail.com>
2022-03-12 19:00:52 -0800

    add fahrenheit/celsius conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const cel = (temp-32) * (5/9);
+  return Number(cel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const fare = temp * 9/5 + 32;
+  return Number(fare.toFixed(1));
 };
 
 // Do not edit below this line
