29c81044c1034b9068b2c46d42d53ec35b02d944
tlamatqui <tlamatqui@protonmail.com>
2022-03-12 17:04:43 -0800

    display sum from provided value range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let value = 0;
 
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    for (i=num1; i<= num2; i++) {
+      value += i
+    }
+  } else {
+    for (i=num2; i<=num1; i++){
+      value += i;
+    }
+  }
+  return value;
 };
 
 // Do not edit below this line
